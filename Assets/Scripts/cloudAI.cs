2bb4d7d61fcd8a0bd9c2cf900889661068b593a0
yep285 <yep2858@gmail.com>
2014-08-02 19:20:52 +0800

    cloudAI.cs

---
+++ Assets/Scripts/cloudAI.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cloudAI : MonoBehaviour {
+
+	public float maxOffset;
+	public float offset;
+
+	private Transform cloud;
+	private float timer;
+	private float nowOffset;
+	private const float timerInterval = 0.01f;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		cloud = transform;
+		maxOffset = 5.0f;
+		offset = 0f;
+		nowOffset = 0f;
+		timer = 0f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void FixedUpdate ()
+	{
+		timer += Time.fixedDeltaTime;
+
+		if (timer > timerInterval) 
+		{
+			timer = 0f;
+			cloud.position = new Vector2(cloud.position.x + offset,cloud.position.y);
+			nowOffset += offset;
+		}
+		/*if (Mathf.Abs( nowOffset ) > maxOffset)
+		{
+			offset *= -1;
+		}*/
+	}
+}
