c57a279a4264d202dc9e99a15a350b8ecf5c57f3
yep285 <yep2858@gmail.com>
2014-08-03 12:05:16 +0800

    FinalCloud

---
+++ Assets/Scripts/CallCloud.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CallCloud : MonoBehaviour {
+	
+	private CloudAI[] clouds;
+	// Use this for initialization
+
+	void Start () 
+	{
+		clouds = gameObject.GetComponentsInChildren<CloudAI> ();
+	}
+
+	/*
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Q)) 
+		{
+			MoveCloud();	
+		}
+		//Debug.Log(conter);
+	}
+*/
+	public void MoveCloud()
+	{
+		/*
+		GameObject.Find("Cloud1").GetComponent <CloudAI> ().MoveCloud();
+		GameObject.Find("Cloud2").GetComponent <CloudAI> ().MoveCloud();
+		GameObject.Find("Cloud3").GetComponent <CloudAI> ().MoveCloud();
+		GameObject.Find("Cloud4").GetComponent <CloudAI> ().MoveCloud();
+		GameObject.Find("Cloud5").GetComponent <CloudAI> ().MoveCloud();
+		GameObject.Find("Cloud6").GetComponent <CloudAI> ().MoveCloud();
+		*/
+		int[] cloudRand = new int[3];
+		for (int i = 0; i < 3; ++i) 
+		{
+			cloudRand[i] = Random.Range(0,5);
+			if( i == 1 )
+			{
+				while( cloudRand[ i ] == cloudRand[ i-1 ] )
+				{
+					cloudRand[i] = Random.Range(0,5);
+				}
+			}
+			if( i == 2 )
+			{
+				while( cloudRand[ i ] == cloudRand[ i-1 ] || cloudRand[ i ] == cloudRand[ i-2 ]  )
+				{
+					cloudRand[i] = Random.Range(0,5);
+				}
+			}
+			clouds[ cloudRand[ i ] ].quitDirection *= -1;	
+		}
+		/*
+		foreach (CloudAI cloud in clouds) {
+				
+			cloud.MoveCloud();
+
+		}
+*/
+		for( int i = 0; i < 6; ++i )clouds[i].MoveCloud();
+	}
+}
--- Assets/Scripts/cloudAI.cs
+++ Assets/Scripts/cloudAI.cs
@@ -1,47 +1,92 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class cloudAI : MonoBehaviour {
-
-	public float maxOffset;
-	public float offset;
+public class CloudAI : MonoBehaviour {
 
 	private Transform cloud;
-	private float timer;
-	private float nowOffset;
-	private const float timerInterval = 0.01f;
 
+	public float maxOffsetRandMin;
+	public float maxOffsetRandMax;
+	private float maxOffset;
+	public float offsetRandMin;
+	public float offsetRandMax;
+	private float offset;
+	private float timer;
+	private float timerForMove;
+	private float nowOffset;
+	private float nowOffsetForMove;
+	public float limit;
+	public float timerInterval;
+	public float timerIntervalForMove;
+	public float quitSpeed;
+	private float rand;
+
+
+	public int quitDirection = 1;
+	private int animationDirection = 1; 
 
 	// Use this for initialization
 	void Start () 
 	{
 		cloud = transform;
-		maxOffset = 5.0f;
-		offset = 0f;
-		nowOffset = 0f;
-		timer = 0f;
-
+		/*maxOffsetRandMin = 0.5f;
+		maxOffsetRandMax = 1.0f;
+		offsetRandMin = 0.01f;
+		offsetRandMax = 0.02f;
+		limit = 10f;
+		timerInterval = 0.01f;
+		timerIntervalForMove = 0.05f;
+		quitSpeed = 0;*/
+		nowOffset = 0;
+		timer = 0;
+		timerForMove = 0;
+		maxOffset = Random.Range(maxOffsetRandMin,maxOffsetRandMax);
+		offset = Random.Range(offsetRandMin,offsetRandMax);
+		//shm = Mathf.Cos (offset) + Mathf.Sin (offset);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		
 	}
+	
+	public void MoveCloud()
+	{	
+		StartCoroutine ("CloudAnimation");
+	}
+	
+	IEnumerator CloudAnimation()
+	{
+		for (;;) 
+		{
+			timerForMove += Time.fixedDeltaTime;
+			timerForMove = 0f;
+			cloud.position = new Vector2 (cloud.position.x + offset*quitSpeed*quitDirection, cloud.position.y);
+			nowOffsetForMove += offset;
 
+			if (nowOffsetForMove > limit)
+			{
+				Destroy(gameObject);
+			}
+			yield return null;
+		}
+	}
+	
 	void FixedUpdate ()
 	{
 		timer += Time.fixedDeltaTime;
-
+		
 		if (timer > timerInterval) 
 		{
 			timer = 0f;
-			cloud.position = new Vector2(cloud.position.x + offset,cloud.position.y);
-			nowOffset += offset;
+			cloud.position = new Vector2(cloud.position.x,cloud.position.y + offset*animationDirection);
+			nowOffset += offset*animationDirection;
 		}
-		/*if (Mathf.Abs( nowOffset ) > maxOffset)
-		{
-			offset *= -1;
-		}*/
+		
+		if (Mathf.Abs( nowOffset ) > maxOffset)
+		{	
+			animationDirection *= -1;
+		}
 	}
 }
